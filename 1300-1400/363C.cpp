a5aff15899bfd167e2f93a05477ae89a949feb83
Vedant Joshi <vedantj76@gmail.com>
2024-05-22 15:21:16 +0530

    Update 363C.cpp

--- 1300-1400/363C.cpp
+++ 1300-1400/363C.cpp
@@ -1,1 +1,55 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    string temp;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+
+        if (temp.length() >= 2)
+        {
+            if (temp[temp.length() - 1] == s[i] && temp[temp.length() - 2] == s[i])
+            {
+                continue;
+            }
+            else
+            {
+                temp += s[i];
+            }
+        }
+        else
+        {
+            temp += s[i];
+        }
+    }
+
+    string ans;
+
+    for (int i = 0; i < temp.length(); i++)
+    {
+        if (ans.length() >= 3)
+        {
+            if (ans[ans.length() - 1] == temp[i] && ans[ans.length() - 2] == ans[ans.length() - 3])
+            {
+                continue;
+            }
+            else
+            {
+                ans += temp[i];
+            }
+        }
+        else
+        {
+            ans += temp[i];
+        }
+    }
+
+    cout << ans << endl;
+}
